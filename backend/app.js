afef32f6021f89f44bcd163c0418f5f504e4a539
Arockiaraj J <arockiaraj4693@gmail.com>
2026-02-13 11:58:09 +0530

    FINAL COMMIT 5.5

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://todos-7gv2.vercel.app", "http://localhost:3000"],
+    origin: [
+      "https://todos-7gv2.vercel.app",
+      "http://localhost:3000",
+      "http://10.94.59.76:3000",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
